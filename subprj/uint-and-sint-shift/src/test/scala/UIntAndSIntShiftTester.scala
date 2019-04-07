9c277be3afcb6d043bcf310d8eb103cd23d05a63
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-04-07 15:30:16 +0900

    テストケースを1つ追加

--- subprj/uint-and-sint-shift/src/test/scala/UIntAndSIntShiftTester.scala
+++ subprj/uint-and-sint-shift/src/test/scala/UIntAndSIntShiftTester.scala
@@ -7,14 +7,17 @@ import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
   */
 class ShiftTestModule extends Module {
   val io = IO(new Bundle {
+    val uintIn = Input(UInt(32.W))
     val uintOut = Output(UInt(32.W))
+    val sintOutFromUIntIn = Output(SInt(32.W))
     val sintOut = Output(SInt(32.W))
     val sintToUIntOut = Output(UInt(32.W))
   })
 
-  io.uintOut := BigInt("deadbeaf", 16).asUInt() >> 8.U // UIntの値を右シフト
-  io.sintOut := (0xdeadbeaf.S >> 8.U)                  // SIntの値を右シフト
-  io.sintToUIntOut := (0xdeadbeaf.S >> 8.U).asUInt()   // SIntの値を右シフトした後にUIntに変換
+  io.uintOut := BigInt("deadbeaf", 16).asUInt() >> 8          // UIntの値を右シフト
+  io.sintOut := (0xdeadbeaf.S >> BigInt(8))                   // SIntの値を右シフト
+  io.sintToUIntOut := (0xdeadbeaf.S >> 8.U).asUInt()          // SIntの値を右シフトした後にUIntに変換
+  io.sintOutFromUIntIn := io.uintIn.asTypeOf(SInt(32.W)) >> 8 // シフト量はInt/BigInt/UIntならOK
 }
 
 class UIntAndSIntShiftTester extends ChiselFlatSpec {
@@ -50,5 +53,14 @@ class UIntAndSIntShiftTester extends ChiselFlatSpec {
     }
   }
 
+  it should "UIntの入力をSIntに変換してシフトすれば算術シフトになる" in {
+    Driver.execute(Array(""), () => new ShiftTestModule) {
+      c => new PeekPokeTester(c) {
+        poke(c.io.uintIn, 0xdeadbeafL)
+        println(s"0x${peek(c.io.sintToUIntOut).toInt.toHexString}")
+        expect(c.io.sintToUIntOut, 0xfdeadbe)
+      }
+    }
+  }
 }
 
