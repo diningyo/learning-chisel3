c8671e2deb8d789be02c31c321423840499d81f2
diningyo <diningyo.kpuku.jougeki@gmail.com>
2019-10-21 11:07:24 +0900

    AsyncResetの確認コード

---
+++ subprj/chisel-3.2.0/src/main/scala/TrialAsyncReset.scala
@@ -0,0 +1,36 @@
+
+import chisel3._
+import chisel3.internal.naming.chiselName
+import chisel3.util._
+
+@chiselName
+class TrialAsyncReset extends Module {
+  val io = IO(new Bundle {
+    val a = Input(Bool())
+    val b = Input(UInt(2.W))
+    val out_a = Output(Bool())
+    val out_b = Output(Bool())
+    val out_aa = Output(Bool())
+  })
+
+  val r_a = withReset(reset.asAsyncReset()) {
+    RegInit(0.U(8.W))
+  }
+
+  r_a := io.a
+
+  withReset(reset.asAsyncReset()) {
+    val r_b = RegInit(2.U)
+    val r_c = RegNext(io.a, 0.B)
+
+    r_b := io.b
+    io.out_b := r_b
+    io.out_aa := r_c
+  }
+
+  io.out_a := r_a
+}
+
+object ElaborateTrialAsyncReset extends App {
+  Driver.execute(args, () => new TrialAsyncReset)
+}
