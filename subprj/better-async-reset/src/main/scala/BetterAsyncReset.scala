05a2fbeb76cc9288a5b4fa2683a57de25efe3465
diningyo <diningyo.kpuku.jougeki@gmail.com>
2020-04-07 23:14:09 +0900

    インスタンス次のミックスインでも普通に使える

--- subprj/better-async-reset/src/main/scala/BetterAsyncReset.scala
+++ subprj/better-async-reset/src/main/scala/BetterAsyncReset.scala
@@ -4,7 +4,8 @@ import chisel3._
 import chisel3.stage._
 import chisel3.util._
 
-class SubModule extends Module with RequireSyncReset {
+//class SubModule extends Module with RequireSyncReset {
+class SubModule extends Module {
   val io = IO(new Bundle {
     val in = Input(Bool())
     val out = Output(Bool())
@@ -13,7 +14,7 @@ class SubModule extends Module with RequireSyncReset {
   io.out := RegNext(io.in, false.B)
 }
 
-class BetterAsyncReset(useAsyncReset: Boolean = false) extends Module with RequireAsyncReset {
+class BetterAsyncReset(useAsyncReset: Boolean = false) extends Module {
   val io = IO(new Bundle {
     val in = Input(Bool())
     val out = Output(Bool())
@@ -27,7 +28,7 @@ class BetterAsyncReset(useAsyncReset: Boolean = false) extends Module with Requi
 }
 
 object Elaborate extends App {
-  Driver.execute(Array("-td=rtl", "-tn=BetterAsyncReset_false"), () => new BetterAsyncReset(false))
+  Driver.execute(Array("-td=rtl", "-tn=BetterAsyncReset_false"), () => new BetterAsyncReset with RequireAsyncReset (false))
   //ChiselStage.execute(Array("-td=rtl", "-tn=BetterAsyncReset_true"),  () => new BetterAsyncReset(true))
   //ChiselMain.stage.execute()
 }
