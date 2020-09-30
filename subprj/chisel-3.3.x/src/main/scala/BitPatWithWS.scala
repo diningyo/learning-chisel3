7a87050816f5f74bd16870d6454724ebc3d71cac
diningyo <diningyo.kpuku.jougeki@gmail.com>
2020-09-30 22:20:39 +0900

    BitPatのサンプルを追加

---
+++ subprj/chisel-3.3.x/src/main/scala/BitPatWithWS.scala
@@ -0,0 +1,18 @@
+
+import ElaborateChiselName.args
+import chisel3._
+import chisel3.stage.ChiselStage
+import chisel3.util._
+
+class BitPatWithWS extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val out = Output(Bool())
+  })
+
+  io.out := io.in === BitPat("b???? 0001")
+}
+
+object ElaborateBitPatWithWS extends App {
+  println((new ChiselStage).emitVerilog(new BitPatWithWS, args))
+}
